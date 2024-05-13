6e6d3ba7df7798102061003a6bd570f98acb5c66
Xin Hao <haoxinst@gmail.com>
2024-05-13 22:51:12 +0800

    Init the CI

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# nebula-ent
+
+## nebula-ent
+
+[![Build Status Badge]][Build Status]
+[![Go Docs Badge]][Go Docs]
+
+[Build Status Badge]: https://github.com/haoxins/nebula-ent/actions/workflows/test.yaml/badge.svg
+[Build Status]: https://github.com/haoxins/nebula-ent/actions/workflows/test.yaml
+[Go Docs Badge]: https://pkg.go.dev/badge/github.com/haoxins/nebula-ent
+[Go Docs]: https://pkg.go.dev/github.com/haoxins/nebula-ent
--- nebula_test.go
+++ nebula_test.go
@@ -1,11 +1,54 @@
 package ent
 
-import "fmt"
+import (
+	"log"
+	"time"
+
+	nebula "github.com/vesoft-inc/nebula-go/v3"
+)
+
+const (
+	address  = "127.0.0.1"
+	port     = 3699
+	username = "root"
+	password = "nebula"
+)
 
 func prepareSpace(spaceName string) error {
-	fmt.Println("prepare space", spaceName)
-	return nil
-}
+	config := nebula.GetDefaultConf()
+	host := nebula.HostAddress{
+		Host: address,
+		Port: port,
+	}
 
-func dropSpace(spaceName string) {
+	pool, err := nebula.NewConnectionPool(
+		[]nebula.HostAddress{host},
+		config,
+		nebula.DefaultLogger{},
+	)
+	if err != nil {
+		return err
+	}
+
+	sess, err := pool.GetSession(username, password)
+	if err != nil {
+		return err
+	}
+
+	conf := nebula.SpaceConf{
+		Name:      spaceName,
+		Partition: 1,
+		Replica:   1,
+		VidType:   "FIXED_STRING(12)",
+	}
+
+	_, err = sess.CreateSpace(conf)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Space created successfully, waiting for 5 seconds ...")
+	time.Sleep(5 * time.Second)
+
+	return nil
 }
